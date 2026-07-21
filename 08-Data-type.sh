7b80105514985049173d62faa6c85f827e7f3f3c
Mahesh Kasapu <mahimystu@gmail.com>
2026-07-22 01:35:14 +0530

    actions

--- 08-Data-type.sh
+++ 08-Data-type.sh
@@ -6,4 +6,7 @@ SUM=$(($NUM1+$NUM2))
 echo "SUM IS : $SUM"
 
 FRUITS=("Apple" "Banana" "Pomo")
-echo "Fruits of : ${FRUITS[@]}"
+echo "Fruits are : ${FRUITS[@]}"
+echo "1st Fruit is  : ${FRUITS[0]}"
+echo "2nd Fruit is: ${FRUITS[1]}"
+echo "3rd Fruit is: ${FRUITS[2]}"
