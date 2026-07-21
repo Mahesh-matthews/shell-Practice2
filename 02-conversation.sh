65c165f1ccdde5198d80c89c674aee1687b7d327
Mahesh Kasapu <mahimystu@gmail.com>
2026-07-21 19:51:29 +0530

    actions

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: hi Suresh, how are you?"
+echo "Suresh: hi Ramesh, I am fine, how are you?"
+echo "Ramesh: I am fine, Thanks for asking, what else?"
+echo "Suresh: Nothing new, Just going on"
---
+++ 03-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1="Ramesh"
+person2="Suresh"
+
+echo "$person1: hi $person2, how are you?"
+echo "$person2: hi $person1, I am fine, how are you?"
+echo "$person1: I am fine, Thanks for asking, what else?"
+echo "$person2: Nothing new, Just going on"
