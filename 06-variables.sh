9da4be308e852b3ef5b3a789d6b131a9b8ba826f
Mahesh Kasapu <mahimystu@gmail.com>
2026-07-22 01:00:15 +0530

    actions

--- 06-variables.sh
+++ 06-variables.sh
@@ -12,5 +12,4 @@ echo "Script Started at: $START_TIME"
 sleep 10 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(($END_TIME-START_TIME))
-
-echo "Script ended at : $END_TIME "
+echo "Script ended at : $TOTAL_TIME"
