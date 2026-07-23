6d4d2f298fc6c73b0ff5a5dce0387579b8a68bcf
Mahesh Kasapu <mahimystu@gmail.com>
2026-07-23 21:03:17 +0530

    actions

--- 12-logs.sh
+++ 12-logs.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-scripting"
+LOGS_FILE="/var/log/shell-scripting/$0.log"
 if [ $USERID -ne 0 ]; then
     echo "You are not running as root."
     exit 1
  fi  
+
+ mkdir -p $LOGS_FOLDER
 
    validate(){
 
@@ -17,11 +21,11 @@ if [ $USERID -ne 0 ]; then
    }
 
     echo "Installing Nginx web server"
-    dnf install nginx -y 
+    dnf install nginx -y &>> $LOGS_FILE
     validate $? "Installing Nginx"
     echo "Installing MySQL database"
-    dnf install mysql -y 
+    dnf install mysql -y &>> $LOGS_FILE
     validate $? "Installing MySQL"
     echo "Installing Node.js"
-    dnf install nodejs -y 
+    dnf install nodejs -y &>> $LOGS_FILE
     validate $? "Installing Node.js"
