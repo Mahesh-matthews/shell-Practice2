6d5a4aaf2f6cfe56c708558dfb8b1f84d5849376
Mahesh Kasapu <mahimystu@gmail.com>
2026-07-23 22:12:09 +0530

    actions

--- 17-colors-02.sh
+++ 17-colors-02.sh
@@ -4,20 +4,30 @@
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-scripting"
 LOGS_FILE="/var/log/shell-scripting/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="e[0m"
+
+
+
 if [ $USERID -ne 0 ]; then
-    echo "You are not running as root." | tee -a $LOGS_FILE
+    echo -e "$R You are not running as root.$N" | tee -a $LOGS_FILE
     exit 1
  fi  
+
+
 
  mkdir -p $LOGS_FOLDER
 
    validate(){
 
     if [ $1 -ne 0 ]; then
-        echo "$2 ... Failure" | tee -a $LOGS_FILE
+        echo -e "$R $2 ... Failure$N" | tee -a $LOGS_FILE
         exit 1
     else
-        echo "$2 ... Success" | tee -a $LOGS_FILE
+        echo -e "$G $2 ... Success$N" | tee -a $LOGS_FILE
     fi
 
    }
@@ -26,10 +36,10 @@ if [ $USERID -ne 0 ]; then
    do 
     dnf list installed $package &>> $LOGS_FILE
     if [ $? -ne 0 ]; then
-        echo "$package is not installed. Installing $package" | tee -a $LOGS_FILE
+        echo -e "$Y $package is not installed. Installing $package$N" | tee -a $LOGS_FILE
         dnf install $package -y &>> $LOGS_FILE
         validate $? "Installing $package"
     else
-        echo "$package is already installed. Skipping $package" | tee -a $LOGS_FILE
+        echo -e "$B $package is already installed. Skipping $package$N" | tee -a $LOGS_FILE
     fi
     done
